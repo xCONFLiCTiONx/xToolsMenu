99274dc44cf5788db434e2f737b2befe73c6c2b6
xCONFLiCTiONx <info@xconflictionx.cc>
2026-09-02 15:23:44 -0500

    fix: Resolve issue in 2 resources with 54 changes

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
 @echo off
 setlocal
+:: Ensure we are in the script's directory
+cd /d "%~dp0"
+
 echo Building xToolsMenu Solutions...
+echo Working Directory: %cd%
 
 :: Locate MSBuild
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
@@ -12,16 +16,34 @@ if "%VS_PATH%"=="" (
     exit /b 1
 )
 
-call "%VS_PATH%\Common7\Tools\VsDevCmd.bat" -arch=x64
+:: Call VsDevCmd but prevent it from changing directory
+call "%VS_PATH%\Common7\Tools\VsDevCmd.bat" -arch=x64 -no_logo
+
+:: Check for solution file
+if not exist "xToolsMenu.slnx" (
+    echo Error: xToolsMenu.slnx not found!
+    dir /b
+    exit /b 1
+)
 
 :: Build solution
 msbuild xToolsMenu.slnx /p:Configuration=Release /p:Platform=x64
 
 :: Build companion exes
 echo Building companion executables...
-cl /nologo /O2 /Fe:x64\Release\Launcher.exe src\Launcher.cpp /link /SUBSYSTEM:WINDOWS user32.lib
-cl /nologo /O2 /Fe:x64\Release\AttributesDialog.exe src\AttributesDialog.cpp /link /SUBSYSTEM:WINDOWS user32.lib dwmapi.lib advapi32.lib shell32.lib
+if not exist "x64\Release" mkdir "x64\Release"
+
+if exist "src\Launcher.cpp" (
+    cl /nologo /O2 /Fe:x64\Release\Launcher.exe src\Launcher.cpp /link /SUBSYSTEM:WINDOWS user32.lib
+) else (
+    echo Error: src\Launcher.cpp not found!
+)
+
+if exist "src\AttributesDialog.cpp" (
+    cl /nologo /O2 /Fe:x64\Release\AttributesDialog.exe src\AttributesDialog.cpp /link /SUBSYSTEM:WINDOWS user32.lib dwmapi.lib advapi32.lib shell32.lib
+) else (
+    echo Error: src\AttributesDialog.cpp not found!
+)
 
 echo Build complete!
-pause
 endlocal
